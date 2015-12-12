9435cfbb48a474d4b5734b52bb50d3ab7ce71e89
boss <abhichet.ketsiri@gmail.com>
2015-12-12 17:06:53 +0700

    add spring boot

---
+++ src/main/java/com/example/game/Main.java
@@ -0,0 +1,26 @@
+package com.example.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Admin on 12/10/2015.
+ */
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Main {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+}
